af460e2961505f4fda5d400087646ff2112df205
andnil01 <andreasnilsen.01@outlook.com>
2025-01-30 13:24:49 +0100

    v2

--- discgolfapp/backend/src/controllers/userController.js
+++ discgolfapp/backend/src/controllers/userController.js
@@ -67,6 +67,7 @@ const loginUser = async (req, res) => {
     const { email, password } = req.body;
     try {
         const { accessToken, refreshToken, displayName } = await userService.loginUser(email, password);
+        res.status(200).json({ accessToken, refreshToken, displayName });
     } catch (error) {
         res.status(error.statusCode).json({ error: error.message });
     }
