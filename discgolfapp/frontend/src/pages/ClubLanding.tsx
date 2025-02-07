cc32b344c0c099bb3654e3deabbdc5439a7c5a5c
Ibrahim Queeum <i.m.q@hotmail.com>
2025-02-07 16:10:45 +0100

    Klubb landing og komponentene
    Klubb landing og 2 komp

--- discgolfapp/frontend/src/pages/ClubLanding.tsx
+++ discgolfapp/frontend/src/pages/ClubLanding.tsx
@@ -1,12 +1,40 @@
-'use client'
+import { useState } from 'react';
 
-export default function ClubLanding() {
-    
+import '../app/globals.css';
+import MemberBenefit from '@/components/clubpage/memberbenefit';
+import Clublist from '@/components/clubpage/clublistmap';
+import Navbar from '../components/navbar';
+import Footer from '@/components/footer';
+import Login from '@/components/login';
+import Register from '@/components/register';
+
+
+const Clublanding = () => {
+
+const [popupType, setPopupType] = useState<'login' | 'register' | null>(null);
+const toggleLoginPopup = () => setPopupType(popupType === 'login' ? null : 'login');
+const toggleRegisterPopup = () => setPopupType(popupType === 'register' ? 'login' : 'register');
+const closePopup = () => setPopupType(null);
+  
     return (
         <div>
-            <h1>ClubLanding</h1>
-            Work in progress
-        </div>
-    )
+            <Navbar toggleLoginPopup={toggleLoginPopup}/>
+            <div className="flex flex-wrap justify-left gap-3 min-h-screen">
+                <div className="w-96 h-72 p-4 bg-transparen  rounded  items-center justify-center shadow-none">
+                    <MemberBenefit />
+                </div>
+                <div className="w-97 h-72 p-4 bg-transparen  rounded  items-center justify-center shadow-none">
+                    <Clublist />
+                </div>
 
+            </div>
+            {popupType === 'login' && (
+            <Login togglePopup={toggleLoginPopup} toggleRegisterPopup={toggleRegisterPopup} closePopup={closePopup} />
+            )}
+            {popupType === 'register' && <Register togglePopup={toggleRegisterPopup} />}
+            <Footer />
+        </div>
+    );
 }
+
+export default Clublanding;
