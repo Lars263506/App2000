debff7259914e04a2a9c5213e03fc66909d78eaa
Lars Andreas Strand <263506@usn.no>
2025-02-08 21:32:47 +0100

    Fixed optionalAuth

--- discgolfapp/backend/src/middleware/optionalauth.js
+++ discgolfapp/backend/src/middleware/optionalauth.js
@@ -11,7 +11,7 @@ const optionalAuth = (req, res, next) => {
     if (req.header("Authorization")) {
         passport.authenticate('jwt', { session: false }, (err, user, info) => {
             if (err) return next(err);
-            if (user) req.user = user;
+            req.user = user;
             return next();
         })(req, res, next);
     } else {
