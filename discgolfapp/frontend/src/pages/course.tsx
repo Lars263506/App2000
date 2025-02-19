ed7bdf4f313c3c3be68d1ff0cebfdf705384f339
Lars Andreas Strand <263506@usn.no>
2025-02-19 11:03:17 +0100

    Made empty field page
    Page for course information is now up, but contains no components yet. Can be navigated to from front page.

---
+++ discgolfapp/frontend/src/pages/course.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+import '../app/globals.css';
+import Navbar from '../components/navbar';
+import Footer from '@/components/footer';
+import Login from '@/components/login';
+import Register from '@/components/register';
+
+const Field = () => {
+
+const [popupType, setPopupType] = useState<'login' | 'register' | null>(null);
+const toggleLoginPopup = () => setPopupType(popupType === 'login' ? null : 'login');
+const toggleRegisterPopup = () => setPopupType(popupType === 'register' ? 'login' : 'register');
+const closePopup = () => setPopupType(null);
+
+    return (
+        <div>
+            <Navbar toggleLoginPopup={toggleLoginPopup}/>
+            <div className="flex flex-wrap justify-center gap-3">
+
+            </div>
+            {popupType === 'login' && (
+            <Login togglePopup={toggleLoginPopup} toggleRegisterPopup={toggleRegisterPopup} closePopup={closePopup} />
+            )}
+            {popupType === 'register' && <Register togglePopup={toggleRegisterPopup} />}
+            <div className='absolute bottom-0 w-full'>
+                <Footer />
+            </div>
+        </div>
+    );
+}
+
+export default Field;
