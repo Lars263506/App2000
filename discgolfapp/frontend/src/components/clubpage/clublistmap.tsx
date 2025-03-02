ed201e3249151b36b6e4d7f0ad387b55e7c6305b
andnil01 <andreasnilsen.01@outlook.com>
2025-03-02 12:39:51 +0100

    gjort tekst sort
    endret fort der teksten er sort på kart

--- discgolfapp/frontend/src/components/clubpage/clublistmap.tsx
+++ discgolfapp/frontend/src/components/clubpage/clublistmap.tsx
@@ -81,7 +81,7 @@ const ClubListmap = () => {
       </div>
       {/* Map */}
       <div className="w-full md:w-1/2 bg-gray-200 p-4 rounded-xl shadow">
-        <h2 className="text-xl font-bold">Kart</h2>
+        <h2 className="text-xl font-bold text-black">Kart</h2>
         <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string}>
           <GoogleMap
             onLoad={(map) => {
