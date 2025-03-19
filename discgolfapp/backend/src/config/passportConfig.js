bf3b094e4939dbba42cc20dd2b2acbcf21683f0a
andnil01 <andreasnilsen.01@outlook.com>
2025-03-19 15:47:57 +0100

    fjernet facebook og google
    tatt bort alle rester som finnes av facebook og google log inn

--- discgolfapp/backend/src/config/passportConfig.js
+++ discgolfapp/backend/src/config/passportConfig.js
@@ -1,7 +1,5 @@
 import passport from 'passport'
 import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt'
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20'
-import { Strategy as FacebookStrategy } from 'passport-facebook'
 import User from '../models/User.js'
 
 /**
