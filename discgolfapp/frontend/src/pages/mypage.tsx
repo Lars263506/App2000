f531d565ac209af92a607702e1727ff870e84be8
Lars Andreas Strand <263506@usn.no>
2025-03-19 14:57:42 +0100

    Small mypage fix
    imageFile hook not needed

--- discgolfapp/frontend/src/pages/mypage.tsx
+++ discgolfapp/frontend/src/pages/mypage.tsx
@@ -13,7 +13,6 @@ const MyPage = () => {
   const { popupType, toggleLoginPopup, toggleRegisterPopup, closePopup } = usePopup();
   const [user, setUser] = useState<User | null>(null);
   const [profileImage, setProfileImage] = useState<string | null>(null);
-  const [imageFile, setImageFile] = useState<File | null>(null);
   const [clubs, setClubs] = useState<any[]>([]);
   const router = useRouter();
 
@@ -46,7 +45,7 @@ const MyPage = () => {
   // Andre useEffect: Henter klubbene etter at brukerdata er hentet
   useEffect(() => {
     const fetchClubs = async () => {
-      if (!user) return; 
+      if (!user) return;
 
       try {
         const accessToken = localStorage.getItem('accessToken');
@@ -70,13 +69,12 @@ const MyPage = () => {
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
-      setImageFile(file);
       const reader = new FileReader();
       reader.onloadend = () => {
         const imageUrl = reader.result as string;
         setProfileImage(imageUrl);
-        
-        handleSaveProfileImage(file); 
+
+        handleSaveProfileImage(file);
       };
       reader.readAsDataURL(file);
     }
@@ -107,9 +105,9 @@ const MyPage = () => {
   };
 
   const handleClubClick = (clubId: string) => {
-    router.push(`/clubpage?clubId=${clubId}`); 
+    router.push(`/clubpage?clubId=${clubId}`);
   };
-  
+
 
   if (!user) {
     return (
@@ -131,7 +129,7 @@ const MyPage = () => {
       <div className="flex-grow bg-gray-100 p-8 flex flex-col items-center">
         <h1 className="text-4xl font-extrabold mt-6 text-gray-800">Min Side</h1>
         <h2 className="text-2xl text-gray-700 mb-8">Velkommen, {user.displayName ?? "Ukjent"}!</h2>
-        
+
         {/* Profilbilde med redigeringsikon */}
         <div className="relative">
           {profileImage ? (
@@ -177,8 +175,8 @@ const MyPage = () => {
                 <ul>
                   {clubs.map((club) => (
                     <li key={club._id} className="mb-2">
-                      <button 
-                        onClick={() => handleClubClick(club._id)} 
+                      <button
+                        onClick={() => handleClubClick(club._id)}
                         className="text-blue-500 hover:underline">
                         {club.name}
                       </button>
