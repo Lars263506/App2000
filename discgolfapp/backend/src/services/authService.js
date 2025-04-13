fc44798c54fd738e5dd4e88c91f2e34f8cdedb18
Lars <263506@usn.no>
2025-04-13 14:30:53 +0200

    Fixed expiration time for refresh

--- discgolfapp/backend/src/services/authService.js
+++ discgolfapp/backend/src/services/authService.js
@@ -8,8 +8,11 @@ const refreshSession = async (userId) => {
 
         const payload = { id: user._id, role: user.role }
 
-        const newAccessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '10s' })
-        const newRefreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: '7d' })
+        const accessExpiration = process.env.JWT_ACCESS_EXPIRATION || '1h'
+        const refreshExpiration = process.env.JWT_REFRESH_EXPIRATION || '7d'
+
+        const newAccessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: accessExpiration })
+        const newRefreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: refreshExpiration })
 
         return { newAccessToken, newRefreshToken }
     } catch (err) {
