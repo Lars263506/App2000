ffa88689ebef4ca6c19752696840d547a8b5d226
Adrian Johansen <adrianjohansen88@outlook.com>
2025-04-14 11:26:18 +0200

    UpdateMember.js
    Added position and verv so it will be updated.

--- discgolfapp/backend/src/models/Member.js
+++ discgolfapp/backend/src/models/Member.js
@@ -15,12 +15,22 @@ const memberSchema = new mongoose.Schema({
     type: String,
     required: true,
     enum: ['member', 'clubowner', 'admin'],
-    default: 'member' },
-    profilePicture: {
-        type: String,
-        default: ''
-    }
+    default: 'member'
+  },
+  profilePicture: {
+    type: String,
+    default: ''
+  },
+  position: {
+    type: String,
+    default: ''
+  },
+  verv: {
+    type: String,
+    default: ''
+  }
 });
+
 
 const Member = mongoose.model('Member', memberSchema);
 
