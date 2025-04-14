099356a33e4e89ecca248b7b2a2fedbc63a10839
Adrian Johansen <adrianjohansen88@outlook.com>
2025-04-14 10:57:30 +0200

    Update Verv
    Added PATCH to update position.

--- discgolfapp/backend/src/server.js
+++ discgolfapp/backend/src/server.js
@@ -25,7 +25,7 @@ const app = express();
 // Middleware to allow cross-origin requests
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
